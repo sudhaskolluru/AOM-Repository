da36b1ebafb6b52bdfc9be9593ce7a2414cb0076
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2022-03-23 09:25:52 +0530

    Testing

---
+++ Demo Objects/Temp/XXTRINITI_DEMO_ITEMS_VIEW.sql
@@ -0,0 +1,9 @@
+  CREATE OR REPLACE VIEW XXTRINITI_DEMO_ITEMS_VIEW AS
+   SELECT ITEM_NUMBER,   
+    DESCRIPTION,          
+    TEMPLATE_NAME,            
+    ORGANIZATION_CODE,          
+    CREATION_DATE,             
+    CREATED_BY,                                  
+    PROCESS_FLAG,               
+    ERR_MESSAGE from XXTRINITI_DEMO_ITEMS_TABLE;
---
+++ Demo Objects/Temp/XXTRINITI_PF_DEMO_ITEMS_TABLE.sql
@@ -0,0 +1,11 @@
+ CREATE TABLE XXTRINITI_PF_DEMO_ITEMS_TABLE
+  (
+    ITEM_NUMBER                 NUMBER,
+    DESCRIPTION                 VARCHAR2(240),
+    TEMPLATE_NAME               VARCHAR2(240),
+    ORGANIZATION_CODE           VARCHAR2(240),
+    CREATION_DATE               DATE,
+    CREATED_BY                  NUMBER,                  
+    PROCESS_FLAG                VARCHAR2(1),
+    ERR_MESSAGE                 VARCHAR2(1000)
+  ); 
