04901e4c6e4d3d69e37bfdac10891b23ef3d3656
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2023-02-20 12:59:49 +0530

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -1,4 +1,10 @@
-CREATE TABLE temp1 (
+CREATE TABLE APPS.temp1 (
+    col1 number,
+    col2 varchar2(20),
+    col3 varchar2(20)
+);
+
+CREATE TABLE APPS.temp1 (
     col1 number,
     col2 varchar2(20),
     col3 varchar2(20)
