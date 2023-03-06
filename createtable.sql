126f08c6e7500d84daf0b6cb2a232805a0a70603
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2023-03-06 22:29:59 +0530

    Update createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -15,6 +15,16 @@ CREATE TABLE XXBKP.temp1 (
     col2 varchar2(20),
     col3 varchar2(20)
 );
+CREATE    TABLE XXBKP.temp1 (
+    col1 number,
+    col2 varchar2(20),
+    col3 varchar2(20)
+);
+CREATE  TABLE  XXBKP.temp1 (
+    col1 number,
+    col2 varchar2(20),
+    col3 varchar2(20)
+);
 select * from dual;
 select * from dual;
 
