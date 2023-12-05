92311cd7a4d7518beb71c2dfbe926d0f93353428
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2023-12-05 19:02:32 +0530

    Add files via upload

---
+++ Demo Objects/TPI/AOM_TEST_INSERT_QUERY_AL32UTF8.sql
@@ -0,0 +1,11 @@
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (21,'Bu bir Çin dili Günaydın.',10000.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (22,'ve test nesnesidir Tünaydın.',2005.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (23,'Buenos días Hasta mañana',1008.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (24,'Feliz cumpleaños',2003.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (25,'Hola Buenos días',10010.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (26,'¿Cómo estás? Me gustaría',2009.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (27,'I cant even',10065.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (28,'No big deal',20098.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (29,'你叫什么名字',10098.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (30,'你来自哪里  多少钱',20045.0);
+commit;
---
+++ Demo Objects/TPI/XXTPI_AOM_TST_PRC_AL32UTF8.sql
@@ -0,0 +1,37 @@
+CREATE OR REPLACE PROCEDURE XXTPI.XXTPI_TST_PRC AS
+/* Header: TicketNumber Date User 1.1 */
+
+/******************************************************************************
+   NAME:       XXTPI_AOM_TEST
+   PURPOSE:   This procedure is for demo purpose
+   CREATION DATE:  
+   CREATED BY: Sangeetha Sudha
+   CREATION TICKET NUMBER: 
+   REVISIONS:
+   Ver        Date           Author           Action            Request               Description
+   ---------  ------------  ---------------  --------------    ----------------       ------------------------------------
+   1.1     today        isin                    test                test for AOM--- 
+******************************************************************************/
+begin
+--TR
+dbms_output.put_line('Ç, Ş, Ğ, I, İ, Ö, Ü');
+dbms_output.put_line('Günaydın. Tünaydın. Nasılsın?');
+dbms_output.put_line('Günaydın  Ğ');
+--E
+dbms_output.put_line('Buenos días Hasta mañana');
+dbms_output.put_line('Feliz cumpleaños');
+--ESA
+dbms_output.put_line('Hola Buenos días');
+dbms_output.put_line('¿Cómo estás? Me gustaría');
+--US
+dbms_output.put_line('I cant even');
+dbms_output.put_line('No big deal');
+--ZHS
+dbms_output.put_line('你叫什么名字');
+dbms_output.put_line('你来自哪里  多少钱');
+--Dummy Package Body for AOM test in UAT
+--The line added by Sangeetha Sudha Kolluru for testing
+NULL;
+
+end;
+/
