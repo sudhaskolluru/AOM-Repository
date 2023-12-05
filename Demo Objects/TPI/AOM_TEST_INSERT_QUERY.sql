84d1e26418f1eb6fb3ea08b14cd3de7e9e0c54d8
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2023-12-05 11:23:09 +0530

    Add files via upload

---
+++ Demo Objects/TPI/AOM_TEST_INSERT_QUERY.sql
@@ -0,0 +1,6 @@
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (1,Bu bir Çin dili,100.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (2,ve test nesnesidir,200.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (3,Günaydın.,300.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (4,Tünaydın.,400.0);
+INSERT INTO XXTPI_AOM_TEST_EMP_TBL (emp_no,emp_name,salary) values (5,Nasılsın?,500.0);
+commit;
