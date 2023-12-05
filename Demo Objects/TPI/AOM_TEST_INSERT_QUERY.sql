a4f00ab095c341ecf197ae65d0f23d4e225e9c6f
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2023-12-05 16:36:06 +0530

    Update AOM_TEST_INSERT_QUERY.sql

--- Demo Objects/TPI/AOM_TEST_INSERT_QUERY.sql
+++ Demo Objects/TPI/AOM_TEST_INSERT_QUERY.sql
@@ -1,6 +1,6 @@
-INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (1,'Bu bir Çin dili',100.0);
-INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (2,'ve test nesnesidir',200.0);
-INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (3,'Günaydın',300.0);
-INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (4,'Tünaydın',400.0);
-INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (5,'Nasılsın',500.0);
+INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (6,'Bu bir Çin dili',1000.0);
+INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (7,'ve test nesnesidir',500.0);
+INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (8,'Günaydın',600.0);
+INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (9,'Tünaydın',700.0);
+INSERT INTO XXTPI_AOM_EMP_TEST_TBL (emp_no,emp_name,salary) values (10,'Nasılsın',800.0);
 commit;
