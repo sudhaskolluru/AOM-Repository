ea7309fff71d71b8f910fcf303a53f1f41975a71
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2025-10-08 14:28:26 +0530

    Add files via upload

---
+++ Demo Objects/DBA/allSqlScript.sql
@@ -0,0 +1,31 @@
+INSERT INTO test_tableee (id, name) VALUES (1, 'Alice');
+
+CREATE OR REPLACE PROCEDURE proc_test IS
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Hello World');
+END;
+
+
+drop table test_table;
+-- Create a test table
+CREATE TABLE test_table (
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(50)
+);
+
+-- Insert some sample data
+INSERT INTO test_table (id, name) VALUES (1, 'Alice');
+INSERT INTO test_table (id, name) VALUES (2, 'Bob');
+INSERT INTO test_table (id, name) VALUES (3, 'Mike');
+INSERT INTO test_table (id, name) VALUES (4, 'Ben');
+
+-- Select data
+SELECT * FROM test_table;
+
+-- Update a row
+UPDATE test_table SET name = 'Charlie' WHERE id = 1;
+
+-- Delete a row
+DELETE FROM test_table WHERE id  in (2,4);
+
+SELECT * FROM test_table;
