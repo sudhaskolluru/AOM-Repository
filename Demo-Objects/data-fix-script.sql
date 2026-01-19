173a747868ffb2755db980de04b5e0a6d0ee0793
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2026-01-19 12:26:02 +0530

    Update data-fix-script.sql

--- Demo-Objects/data-fix-script.sql
+++ Demo-Objects/data-fix-script.sql
@@ -1,7 +1,7 @@
 UPDATE ap_invoices_all
 SET    description = 'DEMO DATAFIX APPLIED'
 WHERE  invoice_num = '4321'
-AND    org_id = 204
-AND    NVL(description,'X') <> 'DEMO DATAFIX APPLIED';
+AND    org_id = 204;
+
 
 COMMIT;
