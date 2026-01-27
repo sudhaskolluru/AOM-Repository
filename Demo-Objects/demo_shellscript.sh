a3c139f306e91214fdfcb7afb7bf5feec995f0ed
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2026-01-27 20:45:40 +0530

    Update demo_shellscript.sh

--- Demo-Objects/demo_shellscript.sh
+++ Demo-Objects/demo_shellscript.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-# demo_simple.sh - sample migration script
+# demo_shellscript.sh - sample migration script
 
 SRC_DIR=/tmp/demo_src
 DEST_DIR=$XXTRINITI_TOP/demo_dest
-LOG=/tmp/demo_simple.log
 
 echo "Migration started at $(date)"
+echo "Destination directory: $DEST_DIR"
+
 mkdir -p $DEST_DIR
 cp -r $SRC_DIR/* $DEST_DIR/
+
 echo "Migration completed at $(date)"
+echo "Files migrated to: $DEST_DIR"
 
 exit 0
