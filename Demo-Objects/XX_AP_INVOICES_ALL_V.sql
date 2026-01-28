61b185375d858bde923516954e80a0982d5f1530
sudhaskolluru <93921567+sudhaskolluru@users.noreply.github.com>
2026-01-28 13:57:54 +0530

    Update XX_AP_INVOICES_ALL_V.sql

--- Demo-Objects/XX_AP_INVOICES_ALL_V.sql
+++ Demo-Objects/XX_AP_INVOICES_ALL_V.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE VIEW XX_AP_INVOICES_ALL_V AS
 SELECT invoice_id,
        invoice_num,
-       invoice_amount
+       invoice_amount,
+       invoice_date   -- added column
 FROM   ap_invoices_all;
